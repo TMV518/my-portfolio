cd57e33144d61d1fb25702e79c746bb505f413b8
tmv518 <tmv810@gmail.com>
2023-06-10 16:54:44 -0400

    Changed font and some additional UI

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,30 @@
 import Qualification from "../Qualification";
+import classes from "./Projects.module.css";
 const Projects = () => {
   return (
     <Qualification header={"Projects"}>
-      <h2>Pizza Stop</h2>
-      <h3>Food ordering app using a Firebase backend</h3>
-      <h2>Notes App</h2>
-      <h3>To-do list app created using React.js</h3>
-      <h2>Rolly Ball</h2>
-      <h3>Mobile Game created using Unity3D</h3>
+      <div className={classes["projects-section"]}>
+        <h2>Pizza Stop</h2>
+        <h3>
+          Food ordering app using a Firebase backend
+          <br />
+          <a href="https://tmv518.github.io/pizza-stop/">(Link)</a>
+        </h3>
+        <h2>Notes App</h2>
+        <h3>
+          To-do list app created using React.js
+          <br />
+          <a href="https://tmv518.github.io/notes-app-with-context/">(Link)</a>
+        </h3>
+        <h2>Rolly Ball</h2>
+        <h3>
+          Mobile Game created using Unity3D
+          <br />
+          <a href="https://play.google.com/store/apps/details?id=com.PieFunStudio.RollyBall">
+            (Link)
+          </a>
+        </h3>
+      </div>
     </Qualification>
   );
 };
